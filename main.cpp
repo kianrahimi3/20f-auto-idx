6ed3507d949e584cf864bed9fcfcf212df42c2a4
kianrahimi3 <krahimi@smu.edu>
2020-09-19 01:44:12 -0500

    test

--- main.cpp
+++ main.cpp
@@ -17,14 +17,14 @@ int main(int argc, char** argv) {
     DSVector<int> temp;
     temp.push_back(0);
     temp.push_back(5);
-    cout << temp[0] << endl;
-    cout << temp[1] << endl;
+    outputstream << temp[0] << endl;
+    outputstream << temp[1] << endl;
     temp[1] = 4;
     temp[0] = 1;
-    cout << temp[0] << endl;
-    cout << temp[1] << endl;
+    outputstream << temp[0] << endl;
+    outputstream << temp[1] << endl;
 
-    cout << endl << endl;
+    outputstream << endl << endl;
 
     DSList<int> list;
  //   list.push_front(5);
@@ -34,11 +34,11 @@ list.push_back(03);
 list.push_back(01);
 
 
-cout << list << endl;
-cout << list[0] << endl;
-cout << list[1] << endl;
-cout << list[2] << endl;
-cout << list[3] << endl;
+    outputstream << list << endl;
+    outputstream << list[0] << endl;
+    outputstream << list[1] << endl;
+    outputstream << list[2] << endl;
+    outputstream << list[3] << endl;
 
     //return 0;
 }
